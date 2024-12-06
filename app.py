97f4852b407fe529ba0d7731a1fa9ecb1f0a8bbf
Leonard Eshun <leonardeshun@gmail.com>
2024-12-06 17:01:42 -0500

    first test of cloudformation and app runner

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, jsonify
 from flask.logging import create_logger
 import logging
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 app = Flask(__name__)
 LOG = create_logger(app)
@@ -21,6 +28,22 @@ def hello(name):
     return jsonify(greeting)
 
 
+@app.route("/test_database")
+def test_database():
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+        )
+        return "Connection successful"
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        conn.close()
+
+
 if __name__ == "__main__":
     # app.run(host="127.0.0.1", port=8081, debug=True)
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=5006, debug=True)
